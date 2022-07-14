567e1c61e54007f7c14aea71bd424aff5824d64f
sara <saraahmad2060039@gmail.com>
2022-07-14 09:59:11 +0200

    my first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+let counter = document.getElementById("count");
+let countBtn = document.querySelector('[name = "count"]');
+let saveBtn = document.querySelector('[name = "save"]');
+let resetBtn = document.querySelector('[name = "reset"]');
+
+let sum = 0;
+counter.innerText = 0;
+countBtn.addEventListener("click", () => {
+  let num = parseInt(counter.innerText);
+  sum = num + 1;
+  counter.innerText = sum;
+});
+
+saveBtn.addEventListener("click", () => {
+  window.localStorage.setItem("count", `${sum}`);
+});
+resetBtn.addEventListener("click", () => {
+  counter.innerText = 0;
+});
+
+if (localStorage.getItem("count") !== undefined) {
+  counter.innerText = localStorage.getItem("count");
+}
